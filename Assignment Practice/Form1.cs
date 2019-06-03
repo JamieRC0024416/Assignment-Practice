fdd6dbf86da9190a61bc0ef3a9e81d3d582285a4
RC0024416 <RC0024416@CRO-24-21>
2019-06-03 15:06:43 +0100

    Added Tabs

--- Assignment Practice/Form1.cs
+++ Assignment Practice/Form1.cs
@@ -16,5 +16,23 @@ namespace Assignment_Practice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Calculate the area of a rectangle
+            try
+            {
+                double Width, Height, Area;
+                Width = Convert.ToDouble(textBox1.Text);
+                Height = Convert.ToDouble(textBox2.Text);
+                Area = Width * Height;
+                label1.Text = "area = " + Area;
+            }
+            catch
+            {
+                MessageBox.Show("Type in two numbers");
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+            }
+        }
     }
 }
